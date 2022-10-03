869539127ae63164af63d9eab8816b02e38ec4ca
Tinetort <jkbitp@gmail.com>
2022-10-03 18:01:03 +0300

    last i hope

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const buttons = document.querySelectorAll('.popup-btn')
 const closePopUp = document.getElementById('pop-up-close')
 const popUp = document.getElementById('pop-up')
 const navButton = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.navbar-menu')
 
 buttons.forEach((item) => {
   item.addEventListener('click', (e) => {
@@ -17,4 +16,22 @@ closePopUp.addEventListener('click', () => {
 
 navButton.addEventListener('click', ()=>{
   navMenu.classList.toggle('navbar-menu_active')
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.navbar-menu'),
+  menuItem = document.querySelectorAll('.navbar-menu__link'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('navbar-menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('navbar-menu_active');
+      })
+  })
 })
